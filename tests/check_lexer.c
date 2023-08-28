973a2baee5c2323dc6ebd00579ac46948eca492e
Marvin Frohwitter <marvin.frohwitter@web.de>
2023-08-28 23:27:56 +0200

    feat(tests): Added lexer tests.

---
+++ tests/check_lexer.c
@@ -0,0 +1,29 @@
+#include <stddef.h>
+#include <string.h>
+
+#include "../src/lexer.h"
+#include <check.h>
+
+START_TEST(test_lexer_new) {
+
+  char *content_to_parse = "int j = 0;";
+  size_t len = strlen(content_to_parse);
+  size_t pos = 0;
+
+  Lexer base_lexer;
+  base_lexer.content = content_to_parse;
+  base_lexer.content_lenght = len;
+  base_lexer.position = pos;
+
+  Lexer new_lexer = lexer_new(content_to_parse, len, pos);
+
+  ck_assert(base_lexer.content == new_lexer.content);
+  ck_assert(base_lexer.content_lenght == new_lexer.content_lenght);
+  ck_assert(base_lexer.position != new_lexer.position);
+  ck_assert(1 != 2);
+
+
+}
+END_TEST
+
+int main(void) { return 0; }
