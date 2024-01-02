e133dfe76792f127362619361e529cfa779bd040
Marvin Frohwitter <marvin.frohwitter@web.de>
2024-01-02 17:40:00 +0100

    Added support for reading in external files for testing

--- tests/dev-tests.c
+++ tests/dev-tests.c
@@ -1,14 +1,24 @@
+#define GUTILS_IMPLEMENTATION
+#include "../../gutils/gutils.h"
 #include "../src/lexer.h"
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+typedef struct FileSring {
+  char *items;
+  size_t count;
+  size_t capacity;
+} FileString;
+
 int main(void) {
 
   char *content_to_parse = "int "
                            // "4567L "
                            // "4567ULL "
                            // "47 "
+                           // "437; "
                            // "long "
                            // "long "
                            // "4567. "
@@ -16,17 +26,18 @@ int main(void) {
 
                            // "1234ULLH "
                            // "long "
+                           "long 'a'"
                            // "void \n "
 
                            // "\" Das ist ein string\"hallo "
 
                            // "0xB "
                            // "0xB4 "
-                           // "0XCCBBA "
+                           "0XCCBBA "
                            // "0xCCBBAA88 "
                            // "0xB4812ABDBDF "
                            // // Debug fail tests:
-                           // "1e+4 "
+                           "1e+4"
                            // "3e-6 "
                            // "3e+662337 "
                            // "3e-637 "
@@ -41,43 +52,62 @@ int main(void) {
                            // "## \n"
                            // "### vaois das is\n"
 
-                           "hallo \n"
-                           "/**/    int\n"
-                           // // Debug wrong token tests:
-                           "/* BOB*/a"
-                           "/*/*\" */    int\n"
+                           // "hallo \n"
+                           // "/**/    int\n"
+                           // // // Debug wrong token tests:
+                           // "/* BOB*/a"
+                           // "/*/*\" */    int\n"
 
-                           "/*/*\"\"*/  "
+                           // "/*/*\"\"*/  "
 
-                           "hallo \n"
-                           "// \" */ \n   int"
+                           // "hallo \n"
+                           // "// \" */ \n   int"
 
-                           "// /* luchs*/ \n"
-                           "/* BOB \"was ist los\n\"*/\n"
-                           "/* Luchs   */"
-                           "// \"Haus Hans\"   lasst\" IM string \"\n"
+                           // "// /* luchs*/ \n"
+                           // "/* BOB \"was ist los\n\"*/\n"
+                           // "/* Luchs   */"
+                           // "// \"Haus Hans\"   lasst\" IM string \"\n"
+                           // "// \"Haus Hans\"   lasst\n"
+                           // "// \"Haus Hans\"   Hase \n  \n"
                            "// \"Haus Hans\"   lasst\n"
-                           "// \"Haus Hans\"   Hase \n  \n"
-                           "// \"Haus Hans\"   lasst\n"
-                           "hallo \n"
-                           "420";
+                           "size_t \n"
+                           "0xA\n";
 
   size_t len = strlen(content_to_parse);
+
+  FileString fs = {0};
+  char *filename = "/home/marvin/Entwicklung/c/lexer/src/lexer.c";
+
+  FILE *fd = fopen(filename, "rb");
+  if (!fd) {
+    perror("fopen");
+    return 1;
+  }
+
+  int c;
+  while ((c = fgetc(fd)) != EOF) {
+    char character = c;
+    da_append(&fs, character);
+    // fprintf(stderr, "THE CHARS : [%c]\n", character);
+  }
+  fclose(fd);
+
+  // Lexer lexer = lexer_new(fs.items, fs.count, 0);
   Lexer lexer = lexer_new(content_to_parse, len, 0);
 
+  FileString output = {0};
+
   while (lexer.content_lenght - 1 >= lexer.position) {
-     Token t = lexer_next(&lexer);
-    char *temp;
-    char *tofree_temp = temp = malloc(t.size * sizeof(char));
-    assert(temp != NULL && "Buy more RAM !!");
-    strncpy(temp, t.content, t.size);
-    temp[t.size] = 0;
-    printf("[%s] form type %u\n", temp, t.kind);
-    free(tofree_temp);
+   Token t = lexer_next(&lexer);
+
+    da_append_many(&output, t.content, t.size);
+    da_append(&output, '\0');
+
+    fprintf(stdout, "[%s] form type %u\n", output.items, t.kind);
+
+    output.count = 0;
   }
-  char *h = "Succses";
-  printf("%s\n", h);
+  printf("Succses\n");
 
   return 0;
 }
-
